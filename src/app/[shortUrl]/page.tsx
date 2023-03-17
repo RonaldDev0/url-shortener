2a525e0b863c57892146cf5120904a4b913c37aa
Ronald <ronaldzamoradev@gmail.com>
2023-03-16 19:00:42 -0500

    upgrade performance from redirect's page

--- src/app/[shortUrl]/page.tsx
+++ src/app/[shortUrl]/page.tsx
@@ -1,22 +1,8 @@
-'use client'
 import { supabase } from '@/lib'
-import { useRouter } from 'next/navigation'
-import { useEffect } from 'react'
+import { redirect } from 'next/navigation'
 
-export default function Navigate ({ params: { shortUrl } }: {params: { shortUrl: string }}) {
-  const router = useRouter()
-
-  useEffect(() => {
-    async function redirect () {
-      router.push(await getUrl(shortUrl))
-    }redirect()
-  }, [shortUrl, router])
-}
-
-async function getUrl (shortUrl: string) {
+export default async function Navigate ({ params: { shortUrl } }: {params: { shortUrl: string }}) {
   const { data }: any = await supabase.from('urlShorter').select('*').eq('shortUrl', shortUrl)
   const { url }: any = data[0]
-  const redirect = `https://${url}`
-
-  return redirect
+  redirect(`https://${url}`)
 }
