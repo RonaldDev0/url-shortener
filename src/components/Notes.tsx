fe2f3d2c0175966e4a232ea0a268dd0a3566d2f0
Ronald <ronaldzamoradev@gmail.com>
2023-03-18 22:52:20 -0500

    update alerts

--- src/components/Notes.tsx
+++ src/components/Notes.tsx
@@ -1,8 +1,8 @@
 export default function Notes () {
   return (
     <div className='mt-10 bg-gray-200 p-5 rounded-lg dark:bg-gray-800'>
-      <p>⚠️don't add <b>"https://"</b></p>
-      <p>⚠️don't add "www."</p>
+      <p>⚠️don't add <b className='text-x font-bold'>"https://"</b></p>
+      <p>⚠️don't add <b className='text-x font-bold'>"www."</b></p>
     </div>
   )
 }
