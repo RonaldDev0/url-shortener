d0a4f941d2edbc309f5f0998654a8461cb1b3eb6
Ronald <ronaldzamoradev@gmail.com>
2023-03-18 22:34:27 -0500

    update to copyClipboard

--- src/components/ShortLink.tsx
+++ src/components/ShortLink.tsx
@@ -18,7 +18,7 @@ export default function ShortLink ({ shortURL, setCopied, copied }: props) {
       <p className='text-xl font-bold mt-5'>your new url is: </p>
       <div className='flex gap-5 items-center'>
         <Link href={`/${shortURL}`} className='text-s font-bold text-cyan-600'>{domain + shortURL}</Link>
-        <CopyToClipboard text={domain + shortURL}>
+        <CopyToClipboard text={'www.' + domain + shortURL}>
           <button onClick={() => setCopied(true)} className='bg-cyan-500 text-gray-50 px-2 py-1 rounded-lg hover:bg-cyan-600 transition-all text-lg font-bold'>
             {copied ? 'text copied!!' : 'copy'}
           </button>
